14181bcfab094cc306510ef175838bdf844d8be9
Vinicius <solutiontecforyou@gmail.com>
2021-09-21 19:41:01 -0300

    feat: create GlobalStyle

---
+++ src/styles/global.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #f0f2f5;
+        --red: #E52E4D;
+        --green: #33CC95;
+        --blue: #5429CC;
+        --blue-light: #6933FF;
+        --text-title: #363f5f;
+        --text-body: #969cb2;
+        --shape: #FFFFFF;
+        --light: #FFF;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+
+        transition: all 0.2s;
+    }
+
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialised; 
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
