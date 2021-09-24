21d14a8649a25c8ba15934ea45d83b0fe63ab050
Vinicius <solutiontecforyou@gmail.com>
2021-09-24 20:35:28 -0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import Modal from "react-modal";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
+import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement("#root");
+
 export function App() {
+  const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+    setNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModalOpen() {
+    setNewTransactionModalOpen(false);
+  }
   return (
-    <div className="App">
-      <h1>dtmoney</h1>
-    </div>
+    <TransactionsProvider>
+      <GlobalStyle />
+      <Header {...{ handleOpenNewTransactionModalOpen }} />
+      <Dashboard />
+
+      <NewTransactionModal
+        newTransactionModalOpen={newTransactionModalOpen}
+        handleCloseNewTransactionModalOpen={handleCloseNewTransactionModalOpen}
+      />
+    </TransactionsProvider>
   );
 }
